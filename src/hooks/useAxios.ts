e70d648b4d495505136cdb0290e5302b64620efd
Bikcode <bikcodeh@gmail.com>
2023-12-20 22:10:12 -0500

    format code

--- src/hooks/useAxios.ts
+++ src/hooks/useAxios.ts
@@ -1,14 +1,14 @@
 import { useState, useEffect } from 'react';
-import axios, { AxiosError, AxiosResponse} from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
 
 interface AxiosState<T> {
     loading: boolean;
     data: T | null;
     error: AxiosError | null;
-  }
-  
+}
 
-const useAxios = <T> (url: string) => {
+
+const useAxios = <T>(url: string) => {
     const [state, setState] = useState<AxiosState<T>>({
         loading: true,
         data: null,
